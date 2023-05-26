f548db8cc8411e94db848f6a040112ccd2df3e95
sunnysnake <sunnysnake@qq.com>
2023-05-26 23:40:56 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,9 +437,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Banda ChatGPT Web</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              chat.bandayun.com
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
